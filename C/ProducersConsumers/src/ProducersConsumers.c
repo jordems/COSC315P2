a4e5fa18da96428287bdac3be6fbc1e32a5b1147
dBaiko <dylanbaiko1@hotmail.com>
2020-03-02 15:39:57 -0800

    finished coding

--- C/ProducersConsumers/src/ProducersConsumers.c
+++ C/ProducersConsumers/src/ProducersConsumers.c
@@ -1,36 +1,132 @@
-/*
- ============================================================================
- Name        : ProducersConsumers.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello OpenMP World in C
- ============================================================================
- */
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * Hello OpenMP World prints the number of threads and the current thread id
- */
-int main (int argc, char *argv[]) {
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <time.h>
 
-  int numThreads, tid;
+#define BUFFER_SIZE 500
 
-  /* This creates a team of threads; each thread has own copy of variables  */
-#pragma omp parallel private(numThreads, tid)
- {
-   tid = omp_get_thread_num();
-   printf("Hello World from thread number %d\n", tid);
+void *producer(void * ptr);
+void *consumer(void * ptr);
+int bufferGet();
+void bufferPut(int job);
+int randombyRange(int min, int max);
 
-   /* The following is executed by the master thread only (tid=0) */
-   if (tid == 0)
-     {
-       numThreads = omp_get_num_threads();
-       printf("Number of threads is %d\n", numThreads);
-     }
- }
- return 0;
+pthread_mutex_t mutex;
+sem_t full;
+sem_t empty;
+
+int buffer[BUFFER_SIZE];
+int getIndex = 0;
+int putIndex = 0;
+int count = 0;
+int maxDuration;
+
+int main() {
+	sem_init(&full, 0, 0);
+	sem_init(&empty, 0, BUFFER_SIZE);
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_t producerThread;
+
+	int numConsumers;
+
+	char *message1 = "Thread 1";
+
+	printf("# Consumers: ");
+	fflush(stdout);
+	scanf("%d", &numConsumers);
+
+	printf("\nMax Duration: ");
+	fflush(stdout);
+	scanf("\n%d", &maxDuration);
+
+	pthread_create(&producerThread, NULL, producer, (void*) message1);
+
+	for (int i = 0; i < numConsumers; i++) {
+		pthread_t consumerThread;
+		pthread_create(&consumerThread, NULL, consumer, (void*) message1);
+	}
+
+	pthread_exit(NULL);
 }
 
+void *producer(void *ptr) {
+	printf("\nI am a producer, My id is: %d", pthread_self());
+	fflush(stdout);
 
+	while (true) {
+		int randSleep = randombyRange(1, maxDuration);
+		printf("\nProducer sleeping for %d seconds.", randSleep);
+		fflush(stdout);
+
+		sleep(randSleep);
+
+		int randDuration = randombyRange(1, maxDuration);
+		printf("\nProduced Job of length: %d", randDuration);
+
+		bufferPut(randDuration);
+
+		count++;
+
+	}
+
+	return NULL;
+}
+
+void *consumer(void *ptr) {
+	printf("\nI am a consumer, My id is: %d", pthread_self());
+	fflush(stdout);
+
+	while (true) {
+		int job = bufferGet();
+		clock_t startTime = clock();
+		printf("\nConsumer: %d, Consuming Job of duration: %d seconds", pthread_self(), job);
+		fflush(stdout);
+		sleep(job);
+		clock_t endTime = clock();
+		double total_t = (double) (endTime - startTime) / CLOCKS_PER_SEC;
+		printf("\nConsumer completed job in time: %d seconds.", total_t);
+
+	}
+
+	return NULL;
+}
+
+int bufferGet() {
+	sem_wait(&full);
+	pthread_mutex_lock(&mutex);
+
+	int job = buffer[getIndex];
+	getIndex++;
+	if (getIndex > BUFFER_SIZE - 1) {
+		getIndex = 0;
+	}
+	count--;
+	pthread_mutex_unlock(&mutex);
+	sem_post(&empty);
+	return job;
+}
+
+void bufferPut(int job) {
+	sem_wait(&empty);
+	pthread_mutex_lock(&mutex);
+
+	buffer[putIndex] = job;
+	buffer[putIndex] = job;
+	putIndex++;
+	if (putIndex > BUFFER_SIZE - 1) {
+		putIndex = 0;
+	}
+	count++;
+
+	pthread_mutex_unlock(&mutex);
+	sem_post(&full);
+}
+
+// Generates random from min to max with seed of threadID and current Systime
+int randombyRange(int min, int max) {
+	srand(clock());
+	return (int) (((rand() / (double) RAND_MAX) * (max - min + 1)) + min);
+}
