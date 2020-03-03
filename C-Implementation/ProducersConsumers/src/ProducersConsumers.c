f6f9af18dd17a6237ef07679de69bd4552fb83b4
Jordan <jordems@gmail.com>
2020-03-02 17:35:29 -0800

    Removed unnessesary PThread args, and added some simple C comments

--- C-Implementation/ProducersConsumers/src/ProducersConsumers.c
+++ C-Implementation/ProducersConsumers/src/ProducersConsumers.c
@@ -7,8 +7,8 @@
 
 #define BUFFER_SIZE 500
 
-void *producer(void * ptr);
-void *consumer(void * ptr);
+void *producer();
+void *consumer();
 int bufferGet();
 void bufferPut(int job);
 int randombyRange(int min, int max);
@@ -23,7 +23,11 @@ int putIndex = 0;
 int count = 0;
 int maxDuration;
 
-int main() {
+/**
+ * 	Main
+ */
+int main()
+{
 	sem_init(&full, 0, 0);
 	sem_init(&empty, 0, BUFFER_SIZE);
 	pthread_mutex_init(&mutex, NULL);
@@ -31,8 +35,6 @@ int main() {
 	pthread_t producerThread;
 
 	int numConsumers;
-
-	char *message1 = "Thread 1";
 
 	printf("# Consumers: ");
 	fflush(stdout);
@@ -42,21 +44,27 @@ int main() {
 	fflush(stdout);
 	scanf("\n%d", &maxDuration);
 
-	pthread_create(&producerThread, NULL, producer, (void*) message1);
+	pthread_create(&producerThread, NULL, producer, NULL);
 
-	for (int i = 0; i < numConsumers; i++) {
+	for (int i = 0; i < numConsumers; i++)
+	{
 		pthread_t consumerThread;
-		pthread_create(&consumerThread, NULL, consumer, (void*) message1);
+		pthread_create(&consumerThread, NULL, consumer, NULL);
 	}
 
 	pthread_exit(NULL);
 }
 
-void *producer(void *ptr) {
+/**
+ * 	Producer Thread
+ */
+void *producer()
+{
 	printf("\nI am a producer, My id is: %d", pthread_self());
 	fflush(stdout);
 
-	while (true) {
+	while (true)
+	{
 		int randSleep = randombyRange(1, maxDuration);
 		printf("\nProducer sleeping for %d seconds.", randSleep);
 		fflush(stdout);
@@ -69,38 +77,46 @@ void *producer(void *ptr) {
 		bufferPut(randDuration);
 
 		count++;
-
 	}
 
 	return NULL;
 }
 
-void *consumer(void *ptr) {
+/**
+ * 	Consumer Thread
+ */
+void *consumer()
+{
 	printf("\nI am a consumer, My id is: %d", pthread_self());
 	fflush(stdout);
 
-	while (true) {
+	while (true)
+	{
 		int job = bufferGet();
 		clock_t startTime = clock();
 		printf("\nConsumer: %d, Consuming Job of duration: %d seconds", pthread_self(), job);
 		fflush(stdout);
 		sleep(job);
 		clock_t endTime = clock();
-		double total_t = (double) (endTime - startTime) / CLOCKS_PER_SEC;
+		double total_t = (double)(endTime - startTime) / CLOCKS_PER_SEC;
 		printf("\nConsumer completed job in time: %d seconds.", total_t);
-
 	}
 
 	return NULL;
 }
 
-int bufferGet() {
+/**
+ * Gets a job out of the Bounded Buffer with a manually made `synchronous` method
+ */
+int bufferGet()
+{
 	sem_wait(&full);
 	pthread_mutex_lock(&mutex);
 
 	int job = buffer[getIndex];
 	getIndex++;
-	if (getIndex > BUFFER_SIZE - 1) {
+	if (getIndex > BUFFER_SIZE - 1)
+	{
 		getIndex = 0;
 	}
 	count--;
@@ -109,14 +125,18 @@ int bufferGet() {
 	return job;
 }
 
-void bufferPut(int job) {
+/**
+ * Places job into Bounded Buffer within a manually made `synchronous` method
+ */
+void bufferPut(int job)
+{
 	sem_wait(&empty);
 	pthread_mutex_lock(&mutex);
 
 	buffer[putIndex] = job;
-	buffer[putIndex] = job;
 	putIndex++;
-	if (putIndex > BUFFER_SIZE - 1) {
+	if (putIndex > BUFFER_SIZE - 1)
+	{
 		putIndex = 0;
 	}
 	count++;
@@ -125,8 +145,11 @@ void bufferPut(int job) {
 	sem_post(&full);
 }
 
-// Generates random from min to max with seed of threadID and current Systime
-int randombyRange(int min, int max) {
+/** 
+ * Generates random from min to max with seed of CPU clock
+ */
+int randombyRange(int min, int max)
+{
 	srand(clock());
-	return (int) (((rand() / (double) RAND_MAX) * (max - min + 1)) + min);
+	return (int)(((rand() / (double)RAND_MAX) * (max - min + 1)) + min);
 }
